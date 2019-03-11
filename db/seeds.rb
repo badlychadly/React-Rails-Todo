925cbf9dade9bd8b1afba5a0ac716b092ca4dbf1
badlychadly <chadmontoya21@yahoo.com>
2019-03-11 12:35:46 -0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+list = List.create(title: "groceries")
+
+list.items.build([{name: "milk"}, {name: "cheese"}, {name: "butter"}, {name: "shampoo"}])
